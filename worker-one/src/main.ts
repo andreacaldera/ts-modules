3cf99c2287f33dc3486eab5eddc3466248d4ed5a
Andrea Caldera <andrea.caldera@gmail.com>
2019-08-03 17:01:07 +0100

    promise retry

--- worker-one/src/main.ts
+++ worker-one/src/main.ts
@@ -1,7 +1,28 @@
 import { testFromSharedModule } from 'shared-module'
+import { retryPromise } from './retry-promise'
 
 console.log('Running main on worker-one...')
 
 const result = testFromSharedModule()
 console.log(111, result.fieldOne)
 console.log(222, result.fieldTwo)
+
+export const randomPromise = async (): Promise<number> => {
+  return new Promise<number>((resolve, reject) => {
+    if (Math.round(Math.random() * 10) % 7 === 0) {
+      console.log('Random pass')
+      return resolve(1)
+    } else {
+      console.log('Retry promise failed at attempt')
+      return reject(new Error(`Retry promise error`))
+    }
+  })
+}
+
+retryPromise<number>({ attempts: 20, timeout: 200 })(randomPromise)
+  .then(() => {
+    console.log('Retry promise success')
+  })
+  .catch((error: Error) => {
+    console.error('Retry promise failure', error)
+  })
---
+++ worker-one/src/retry-promise.ts
@@ -0,0 +1,26 @@
+const promiseWait = async (timeout: number) =>
+  new Promise(resolve => {
+    setTimeout(resolve, timeout)
+  })
+
+type RetryPromiseConfig = {
+  attempts?: number
+  timeout?: number
+}
+
+export const retryPromise = <T>(config?: RetryPromiseConfig) => async (
+  f: () => Promise<T>
+): Promise<T> => {
+  const { attempts = 10, timeout = 1000 } = config || {}
+  return new Promise<T>(async (resolve, reject) => {
+    await f()
+      .then(resolve)
+      .catch(async (error: Error) => {
+        if (attempts === 0) {
+          return reject(error)
+        }
+        await promiseWait(timeout)
+        return retryPromise({ attempts: attempts - 1, timeout })(f)
+      })
+  })
+}
